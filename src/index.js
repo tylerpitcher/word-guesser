40e1bc45ae254acdda27829776c713a4c9661eaa
tylerpitcher <tylerpitch2015@gmail.com>
2022-02-24 20:33:37 -0500

    Add starting code

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App(props) {
+	return (
+		<div>
+			Hello
+		</div>
+	);
+}
+
+ReactDOM.render(
+	<App/>, 
+	document.getElementById('root')
+);
